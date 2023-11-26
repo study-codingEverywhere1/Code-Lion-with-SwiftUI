76fe9bbeb774c72aa5559100cb889f6b2ead5ec1
jeonguk29 <jeonguk29@naver.com>
2023-11-26 16:46:24 +0900

    2주차 Woogie 업로드 완료

---
+++ Week02/MovieViewChallenge_Woogie/MovieViewChallenge_Woogie/MovieViewChallenge_WoogieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieViewChallenge_WoogieApp.swift
+//  MovieViewChallenge_Woogie
+//
+//  Created by 정정욱 on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieViewChallenge_WoogieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
